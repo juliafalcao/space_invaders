8f6589c24b504032999867a20c483e8c1b8ee603
Júlia Falcão <juliafsfalcao@hotmail.com>
2018-05-16 10:38:46 -0300

    atualizações em aula (16/05)

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ from PPlay.mouse import *
 WINDOW_SIZE = 500
 BUTTON_WIDTH = 200
 BUTTON_HEIGHT = 80
-SHOT_WIDTH = 4
 SHOT_HEIGHT = 14
 SPACESHIP_WIDTH = 42
+SHOT_WIDTH = 4
 SPACESHIP_HEIGHT = 29
-ENEMY_WIDTH = 25
-ENEMY_HEIGHT = 25
+ENEMY_WIDTH = 40
+ENEMY_HEIGHT = 40
 
 BASE_SPEED = 100
 
@@ -24,11 +24,20 @@ ENEMY_SPACING_Y = 40
 BUTTON_BORDER = 30
 BUTTON_SPACING = 20
 
+ENEMY_LINES = 2
+ENEMY_COLUMNS = 5
+
+EASY = 1
+MEDIUM = 2
+HARD = 3
+CURRENT_DIF = 2
+
 mouse = Mouse()
 keyboard = Keyboard()
 
 def menu():
     menu_window = Window(WINDOW_SIZE, WINDOW_SIZE)
+    menu_window.set_title("Space Invaders!")
     bg = GameImage("img/bg.png")
     botao_jogar = Sprite("img/menu_jogar.png")
     botao_dificuldade = Sprite("img/menu_dificuldade.png")
@@ -39,13 +48,16 @@ def menu():
     botao_dificuldade.set_position(150, 190)
     botao_ranking.set_position(150, 256)
     botao_sair.set_position(150, 328)
+
+    CURRENT_DIF = 2
     
     while True:
         if mouse.is_over_object(botao_jogar) and mouse.is_button_pressed(1):
-            game()
+            game(CURRENT_DIF)
 
         elif mouse.is_over_object(botao_dificuldade) and mouse.is_button_pressed(1):
-            print("DIFICULDADE")
+            CURRENT_DIF = dificuldade()
+            print("DIFICULDADE: " + str(CURRENT_DIF))
 
         elif mouse.is_over_object(botao_ranking) and mouse.is_button_pressed(1):
             print("RANKING")
@@ -61,8 +73,48 @@ def menu():
         botao_sair.draw()
         menu_window.update()
 
+def dificuldade():
+    dif_window = Window(WINDOW_SIZE, WINDOW_SIZE)
+    dif_window.set_title("Space Invaders!")
+    background = GameImage("img/bg.png")
 
-def game():
+    while True:
+        if keyboard.key_pressed("1"):
+            return 1
+
+        if keyboard.key_pressed("2"):
+            return 2
+
+        if keyboard.key_pressed("3"):
+            return 3
+
+        background.draw()
+        dif_window.update()
+
+
+def init_enemies(lines, columns):
+    enemies = []
+
+    for i in range(lines):
+        line = []
+        for j in range(columns):
+            enemy = Sprite("img/enemy.png")
+
+            enemy.set_position(ENEMY_START_X + j * (ENEMY_WIDTH + ENEMY_SPACING_X),
+                               ENEMY_START_Y + i * (ENEMY_HEIGHT + ENEMY_SPACING_Y))
+            line.append(enemy)
+        enemies.append(line)
+
+    return enemies
+
+def draw_enemies(enemies):
+     for line in enemies:
+            for enemy in line:
+                if enemy is not None:
+                    enemy.draw()
+
+
+def game(CURRENT_DIF):
     game_window = Window(WINDOW_SIZE, WINDOW_SIZE)
     game_window.set_title("Space Invaders!")
 
@@ -76,15 +128,7 @@ def game():
 
     shots = []
 
-    enemies = []
-    for i in range(2):
-        line = []
-        for j in range(5):
-            enemy = Sprite("img/enemy.png")
-            enemy.set_position(ENEMY_START_X + j * (ENEMY_WIDTH + ENEMY_SPACING_X),
-                               ENEMY_START_Y + i * (ENEMY_HEIGHT + ENEMY_SPACING_Y))
-            line.append(enemy)
-        enemies.append(line)
+    enemies = init_enemies(ENEMY_LINES, ENEMY_COLUMNS)
 
     enemy_speed = 0.3 * BASE_SPEED
     enemy_move_x = 1
@@ -104,14 +148,17 @@ def game():
         if keyboard.key_pressed("RIGHT"):
             if spaceship.x + SPACESHIP_WIDTH <= WINDOW_SIZE:
                 spaceship.set_position(spaceship.x + spaceship_speed * delta_time, spaceship.y)
+
         elif keyboard.key_pressed("LEFT"):
             if spaceship.x >= 0:
                 spaceship.set_position(spaceship.x - spaceship_speed * delta_time, spaceship.y)
+
         if keyboard.key_pressed("SPACE"):
-            if total_time - last_shot_time >= 0.1:
+            if total_time - last_shot_time > 0.1 * CURRENT_DIF:
                 new_shot = Sprite("img/shot.png")
                 new_shot.set_position(spaceship.x + SPACESHIP_WIDTH / 2, spaceship.y - SHOT_HEIGHT)
                 shots.append(new_shot)
+
                 last_shot_time = total_time
 
         elif keyboard.key_pressed("Q"):
@@ -123,7 +170,7 @@ def game():
         # update shot positions
         for shot in shots:
             shot.set_position(shot.x, shot.y - shot_speed * delta_time)
-            if shot.y <= 0 - SHOT_HEIGHT:
+            if shot.y <= - SHOT_HEIGHT:
                 shots.remove(shot)
 
         # update enemy positions
@@ -157,13 +204,7 @@ def game():
         for shot in shots:
             shot.draw()
 
-        """
-        for line in enemies:
-            for enemy in line:
-                if enemy is not None:
-                    enemy.draw()
-        """
-        
+        draw_enemies(enemies)
         game_window.update()
 
 
